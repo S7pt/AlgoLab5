5f4d1a87069ee7a76a96c1e04eac50be0ce53a4a
S7pt <chaos160702@gmail.com>
2020-12-18 04:41:43 +0200

    Added Rabin-Karp pattern searching algorithm

---
+++ FileInteractor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AlgoLaba5
+{
+    class FileInteractor
+    {
+        public static (string,string) readStringsFromFile(string path)
+        {
+            string text;
+            string pattern;
+            using (StreamReader sr = new StreamReader(path)) {
+                text = sr.ReadLine();
+                pattern = sr.ReadLine();
+                Console.WriteLine("Data successfully extracted");
+            }
+            return (text,pattern);
+                
+        }
+        public static void writeToFile(List<(int,int)> result,string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                if (result.Count == 0)
+                {
+                    sw.WriteLine("There are no substrings matching given pattern");
+                    return;
+                }
+                foreach (var unit in result)
+                {
+                    sw.WriteLine("Found right substring starting at index {0}, ending at {1}", unit.Item1, unit.Item2);
+                }
+            }
+            Console.WriteLine("Data was successfully written to the file");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoLaba5
+{
+    public class Program
+    {
+        const int characters = 256;
+        public static List<(int, int)> rabinKarpAlgorithm(string inputString, string pattern, int binary)
+        {
+            int patternHash = 0;
+            int textHash = 0;
+            int h = 1;
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                patternHash = (characters * patternHash + pattern[i]) % binary;
+                textHash = (characters * textHash + inputString[i]) % binary;
+            }
+            for (int i = 0; i < pattern.Length - 1; i++)
+                h = (h * characters) % binary;
+            int leftEdge = 0;
+            List<(int, int)> indicesOfSubstring = new List<(int, int)>();
+
+            for (int i = 0; i < inputString.Length - pattern.Length; i++)
+            {
+                if (textHash == patternHash)
+                {
+                    for (int j = 0; j < pattern.Length; j++)
+                    {
+                        if (inputString[i + j] != pattern[j])
+                        {
+                            break;
+                        }
+                        if (j == pattern.Length - 1)
+                        {
+                            indicesOfSubstring.Add((leftEdge, leftEdge + pattern.Length - 1));
+                        }
+                    }
+                }
+                if (i < inputString.Length - pattern.Length)
+                {
+                    textHash = ((characters * (textHash - inputString[i] * h) + inputString[i + pattern.Length]) % binary);
+                    if (textHash < 0)
+                    {
+                        textHash += binary;
+                    }
+                }
+                leftEdge++;
+            }
+            return indicesOfSubstring;
+        }
+        static void Main(string[] args)
+        {
+            (string text, string pattern) data = FileInteractor.readStringsFromFile("../../../data_input.txt");
+            int prime = 101;
+            List<(int, int)> result = rabinKarpAlgorithm(data.text, data.pattern, prime);
+            FileInteractor.writeToFile(result, "../../../data_output.txt");
+        }
+    }
+}
